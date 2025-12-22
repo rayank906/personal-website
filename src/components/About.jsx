97b33dbfadacf45100a790f20d9cc61ccbd48a57
Rayan Kamdem <rayankdm@umich.edu>
2025-12-22 04:08:11 -0600

    fixed abstract

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -13,19 +13,14 @@ export const About = () => {
                     <h3 className="text-2xl font-semibold"></h3>
                     <p className="text-muted-foreground">
                         I'm a student at the University of Michigan majoring in Computer Science
-                        and minoring in Mathematics. I'm currently very interested in systems and cloud computing, financial markets,
-                        machine learning and abstract mathematics. In my free time, you'll catch me on the basketball courts, at my desk drawing
+                        and minoring in Mathematics. I'm currently very interested in systems and cloud computing, blockchain technology and 
+                        machine learning. In my free time, you'll catch me on the basketball courts, at my desk drawing
                         or outside reading.
                     </p>
                     <div className="flex flex-col sm:flex-row gap-4 pt-4 justify-center">
                         <a href="#contact" className="cosmic-button ">
                             {" "}
                             Get In Touch
-                        </a>
-                        <a href="/files/resume_rayan.pdf" 
-                        className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
-                        target="_blank">
-                            Resume
                         </a>
                     </div>
                 </div>
